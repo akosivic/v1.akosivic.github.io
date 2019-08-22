49322ef95e2ed9a870e14766215333cb1d65f202
akosivic <vic.salak@gmail.com>
2019-08-22 19:09:34 +0800

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-# akosivic.github.io
---
+++ precache-manifest.1b88b328e05bd8b59bc4987b09bc7a8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d14e4622a9f92021002876c8af5bb821",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d9560e25fb5eee1bfce",
+    "url": "/static/css/main.2b31a41f.chunk.css"
+  },
+  {
+    "revision": "5805630d6b8219a3bf65",
+    "url": "/static/js/2.ca7968d3.chunk.js"
+  },
+  {
+    "revision": "5d9560e25fb5eee1bfce",
+    "url": "/static/js/main.5c0dc716.chunk.js"
+  },
+  {
+    "revision": "aabe189ff1ffb844a309",
+    "url": "/static/js/runtime~main.eecce049.js"
+  },
+  {
+    "revision": "a256f8694c08e539e8c98c28410a04e3",
+    "url": "/static/media/img_avatar.a256f869.jpg"
+  }
+]);
